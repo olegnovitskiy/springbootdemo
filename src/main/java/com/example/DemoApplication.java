c0a98706af7a305cfb6804a71c9016ce9ffc268d
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-06-30 21:59:39 +0300

    Step 2 - Create Reservation Entity

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,24 +2,42 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @SpringBootApplication
 public class DemoApplication {
-	class Foo {}
-	class Bar {}
-
-	@Bean
-	Bar bar() {
-		return new Bar();
-	}
-
-	@Bean
-	Foo foo(Bar bar) {
-		return new Foo();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+}
+
+@Entity
+class Reservations {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public Reservations() {     // why JPA, Why?
+    }
+
+    Reservations(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservations{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
