e86227e5a624b338fc50e5bea5e21177e5cba373
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-06-30 21:54:52 +0300

    Step 1 - Create two beans

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,9 +2,22 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
+	class Foo {}
+	class Bar {}
+
+	@Bean
+	Bar bar() {
+		return new Bar();
+	}
+
+	@Bean
+	Foo foo(Bar bar) {
+		return new Foo();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
